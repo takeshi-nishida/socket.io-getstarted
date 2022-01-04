1caf49509f6df982cfae47bdac40b96b5a3f8e77
takeshi-nishida <takeshi-nishida@users.noreply.github.com>
2022-01-04 18:44:01 +0900

    Emitting Events

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
